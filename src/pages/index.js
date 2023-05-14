bed2b430627a3dcd947ca8e6f1e95366b77076c4
Voltrein-code <mr-voltrein@yandex.ru>
2023-05-14 20:58:18 +0300

    feat: Подготовить сайт к рецензии

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,7 @@ import { anchorScroll, trackScroll } from '../utils/utils';
 anchorScroll('.header__about-link', '.about');
 anchorScroll('.button_type_order', '.order__form');
 anchorScroll('.button_type_scroll', '.header');
+anchorScroll('.button_type_examples', '.products');
+anchorScroll('.lead__more', '.about');
 
 window.addEventListener('scroll', trackScroll);
