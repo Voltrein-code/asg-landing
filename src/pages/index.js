fe3d423d317001aeb01c6e8fed9eea5c2c6ac96b
Voltrein-code <mr-voltrein@yandex.ru>
2023-05-14 12:45:24 +0300

    feat: Добавить кнопку скролла вверх

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
+/* eslint-env browser */
+
 import './index.css';
-import { anchorScroll } from '../utils/utils';
+import { anchorScroll, trackScroll } from '../utils/utils';
 
 anchorScroll('.header__about-link', '.about');
 anchorScroll('.button_type_order', '.order__form');
+anchorScroll('.button_type_scroll', '.header');
+
+window.addEventListener('scroll', trackScroll);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,15 +5,29 @@ const anchorScroll = (anchorSelector, targetSelector) => {
   const anchor = document.querySelectorAll(anchorSelector);
   const target = document.querySelector(targetSelector);
 
-  Array.from(anchor).forEach((el) => [
+  Array.from(anchor).forEach((el) => {
     el.addEventListener('click', (e) => {
       e.preventDefault();
 
       target.scrollIntoView({ behavior: 'smooth' });
-    }),
-  ]);
+    });
+  });
+};
+
+const trackScroll = () => {
+  const scrollButton = document.querySelector('.button_type_scroll');
+
+  const scrolled = window.scrollY;
+  const coords = document.documentElement.clientHeight;
+
+  if (scrolled > coords) {
+    scrollButton.classList.remove('button_hidden');
+  } else {
+    scrollButton.classList.add('button_hidden');
+  }
 };
 
 export {
   anchorScroll,
+  trackScroll,
 };
