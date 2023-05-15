270c147b4ffb0fcc5e51dd16eeef7f29007e9819
Voltrein-code <mr-votlrein@yandex.ru>
2023-05-15 16:40:25 +0300

    fix: Изменить разметку для попапа продукции

---
+++ src/components/PopupWithProduct.js
@@ -0,0 +1,11 @@
+import Popup from "./Popup"
+
+export default class PopupWithProduct extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this.image = this.currentPopup.quarySelector('.popup__image');
+    this.heading = this.currentPopup.quarySelector('.popup__header');
+    this.text = 
+
+  }
+}
