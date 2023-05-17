8a36eec9f8cd99ef7738983b534612fb9939cd02
Voltrein-code <mr-voltrein@yandex.ru>
2023-05-17 13:10:39 +0300

    feat: Поменять цветовую гамму

---
+++ src/utils/data.js
@@ -0,0 +1,15 @@
+const productData = [
+  {
+    cardIcon: './media/macbook-pro-svgrepo-com.svg',
+    cardHeading: 'ИТ консалтинг и инжиниринг',
+    cardSubtitle: 'Оценка текущего состояния ИТ-систем вашего бизнеса и оптимизация ИТ-процессов',
+    cardCheckList: [
+      'Объективная оценка',
+      'Экспертное мнение',
+      'Разработка стратегии',
+    ],
+    cardMore: {
+      
+    }
+  },
+]
