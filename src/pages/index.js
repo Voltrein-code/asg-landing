3f7448440e3102d273e3cefb3a9bdae2850c77e7
Voltrein-code <mr-votlrein@yandex.ru>
2023-05-27 09:01:05 +0300

    feat: Добавить отрисовку карточек из массива

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import Popup from '../components/Popup';
 import CardProduct from '../components/CardProduct';
 import { anchorScroll, trackScroll } from '../utils/utils';
-import productData from '../utils/data';
+import { productData, aboutData } from '../utils/data';
 
 import {
-  popupProduct, cardProductTemplate, productButtonSelector, productCardsContainer,
+  popupProduct,
+  cardProductTemplate,
+  productButtonSelector,
+  productCardsContainer,
+  cardAboutTemplate,
+  aboutButtonSelector,
+  aboutCardsContainer,
 } from '../utils/consts';
+import CardAbout from '../components/CardAbout';
 
 anchorScroll('.header__about-link', '.about');
 anchorScroll('.button_type_order', '.order__form');
@@ -24,19 +31,6 @@ productPopup.setEventListeners();
 const aboutPopup = new Popup(popupProduct);
 aboutPopup.setEventListeners();
 
-// Array.from(document.querySelectorAll('.button_type_product')).forEach((el) => {
-//   el.addEventListener('click', (e) => {
-//     productPopup.open(e);
-//     popupProduct.querySelector('.popup__header').textContent = 'Hello world';
-//   });
-// });
-
-// Array.from(document.querySelectorAll('.card__button')).forEach((el) => {
-//   el.addEventListener('click', (e) => {
-//     aboutPopup.open(e);
-//   });
-// });
-
 function handlerButtonClick(event) {
   productPopup.open(event);
 }
@@ -46,4 +40,10 @@ productData.forEach((element) => {
   const card = new CardProduct(cardProductTemplate, productButtonSelector, handlerButtonClick, element);
 
   productCardsContainer.prepend(card.getCard());
+});
+
+aboutData.forEach((element) => {
+  const card = new CardAbout(cardAboutTemplate, aboutButtonSelector, handlerButtonClick, element);
+
+  aboutCardsContainer.prepend(card.getCard());
 });
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -3,7 +3,10 @@
 const popupProduct = document.querySelector('.popup_type_product');
 const popupAbout = document.querySelector('.popup_type_about');
 const cardProductTemplate = document.querySelector('.card-container_type_product').content;
+const cardAboutTemplate = document.querySelector('.card-container_type_about').content;
 const productButtonSelector = '.button_type_product';
+const aboutButtonSelector = '.card__button';
+const aboutCardsContainer = document.querySelector('.about__cards');
 const productCardsContainer = document.querySelector('.products__cards');
 
 export {
@@ -12,4 +15,7 @@ export {
   cardProductTemplate,
   productButtonSelector,
   productCardsContainer,
+  cardAboutTemplate,
+  aboutButtonSelector,
+  aboutCardsContainer,
 };
